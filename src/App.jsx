6f282f3a0c6c302e30440db171af16d05d3c8504
chromecodes <hameedhussain782@gmail.com>
2023-01-10 23:30:57 +0530

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import Home from "./components/Home";
 import Shop from "./components/Shop";
 import About from "./components/About";
 import Header from "./components/Header";
+import Cart from "./components/Cart";
 
 export default function App() {
+  const [cart, updateCart] = useState([]);
+  const [showCart, updateShowCart] = useState(false);
+
+  const openCart = () => {
+    updateShowCart(true);
+  };
+  const closeCart = () => {
+    updateShowCart(false);
+  };
+
   return (
     <>
       <div className='app bg-slate-100'>
-        <Header />
-
+        {showCart ? <Cart close={closeCart} /> : undefined}
+        <Header open={openCart} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Shop' element={<Shop />} />
